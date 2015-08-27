c65b9c64d3f43f9226231c311c22ace3f6c136e7
Santhanaraman <santhanaraman.ganesh@appfolio.com>
2015-08-27 13:48:37 -0700

    sg - Initial commit for MovieDatabase

---
+++ flix/app/controllers/movies_controller.rb
@@ -0,0 +1,36 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.released
+  end
+  def show
+    @movie = Movie.find(params[:id])
+  end
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+  def update
+    @movie = Movie.find(params[:id])
+    #movie_params = (params[:movie]).permit(:title,:description,:rating,:released_on,:total_gross)
+    @movie.update(movie_params)
+    redirect_to(@movie)
+  end
+
+  def new
+    @movie = Movie.new
+  end
+  def create
+    #movie_params = (params[:movie]).permit(:title,:description,:rating,:released_on,:total_gross)
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to(@movie)
+  end
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to(movies_url)
+  end
+  private
+      def movie_params
+        (params[:movie]).permit(:title,:description,:rating,:released_on,:total_gross,:cast,:director,:duration,:image_file_name)
+      end
+end
